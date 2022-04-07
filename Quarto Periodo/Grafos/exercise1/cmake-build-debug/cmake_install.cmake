35fbb6a318e42c04fab3c2fd753afce4f1747e43
Marcos Ani Cury Vinagre Silva <65677865+MarcosAniCury@users.noreply.github.com>
2022-04-06 22:53:16 -0300

    Restart of the project

--- Quarto Periodo/Grafos/exercise1/cmake-build-debug/cmake_install.cmake
+++ Quarto Periodo/Grafos/exercise1/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Marcos/CLionProjects/GrafosExercise1
+# Install script for directory: C:/Users/Marcos/Documents/GitHub/Trabalhos-da-Faculdade/Quarto Periodo/Grafos/exercise1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Marcos/CLionProjects/GrafosExercise1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Marcos/Documents/GitHub/Trabalhos-da-Faculdade/Quarto Periodo/Grafos/exercise1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- Quarto Periodo/Grafos/exercise1/main.cpp
+++ Quarto Periodo/Grafos/exercise1/main.cpp
@@ -6,70 +6,81 @@ using namespace std;
 #define TABELA_ASCII_CONVERTER 65
 #define NUM_CHARACTER_IN_PAIR_EDGES 4
 
-class MatrizAdjacencia {
-    int matriz[MAX_VERTICE][MAX_VERTICE];
-    public:
-        MatrizAdjacencia(char pairsEdges[], int nEdges) {
-            for(int i = 0;i < nEdges;i++){
-
-            }
-        }
-};
-
-class PairsEdges {
-    char edge1;
-    char edge2;
+//class MatrizAdjacencia {
+//    int matriz[MAX_VERTICE][MAX_VERTICE];
+//    public:
+//        MatrizAdjacencia(char pairsEdges[], int nEdges) {
+//            for(int i = 0;i < nEdges;i++){
+//
+//            }
+//        }
+//};
+//
+//class PairsEdges {
+//    char edge1;
+//    char edge2;
+//public:
+//    PairsEdges(string pairsEdges) {
+//        try {
+//            if (pairsEdges.size() == NUM_CHARACTER_IN_PAIR_EDGES) {
+//                if (pairsEdges[1] == '-' && pairsEdges[2] == '>') {
+//                    edge1 = pairsEdges[0];
+//                    edge2 = pairsEdges[3];
+//                } else {
+//                    string initMsg = "Edge not recognized string \"";
+//                    string finalMsg = "\" invalided";
+//                    string msg = initMsg + pairsEdges[1] + pairsEdges[2] + finalMsg;
+//                    throw msg;
+//                }
+//            } else {
+//                string msg = "Numbers of characters in pairs has to be 4";
+//                throw msg;
+//            }
+//        } catch (const string msg) {
+//            cerr << msg << endl;
+//            exit(0);
+//        }
+//    };
+//
+//    int getEdge1Int() {
+//        return tableASCIIConverterToInt(edge1);
+//    }
+//
+//    int getEdge2Int() {
+//        return tableASCIIConverterToInt(edge2);
+//    }
+//
+//    int tableASCIIConverterToInt(char caracter) {
+//        return (int) caracter - TABELA_ASCII_CONVERTER;
+//    }
+//};
+class GrafosToUse {
 public:
-    PairsEdges(string pairsEdges) {
-        try {
-            if (pairsEdges.size() == NUM_CHARACTER_IN_PAIR_EDGES) {
-                if (pairsEdges[1] == '-' && pairsEdges[2] == '>') {
-                    edge1 = pairsEdges[0];
-                    edge2 = pairsEdges[3];
-                } else {
-                    string initMsg = "Edge not recognized string \"";
-                    string finalMsg = "\" invalided";
-                    string msg = initMsg + pairsEdges[1] + pairsEdges[2] + finalMsg;
-                    throw msg;
-                }
-            } else {
-                string msg = "Numbers of characters in pairs has to be 4";
-                throw msg;
-            }
-        } catch (const string msg) {
-            cerr << msg << endl;
-            exit(0);
-        }
-    };
-
-    int getEdge1Int() {
-        return tableASCIIConverterToInt(edge1);
-    }
-
-    int getEdge2Int() {
-        return tableASCIIConverterToInt(edge2);
-    }
-
-    int tableASCIIConverterToInt(char caracter) {
-        return (int) caracter - TABELA_ASCII_CONVERTER;
-    }
+    void
 };
 
-void basicMessage() {
-    cout << "Bem vindo ao sistema de armazenamento de grafos" << endl;
-    string msg = "Escolha entre as opcoes abaixo\n1)Grafo direcionado nao-ponderado\n2)Grafo nao-direcionado nao-ponderado\n3)Grafo direicionado ponderado\n4)Grafo nao-direcionado ponderado\n\n0)Para sair";
-    cout << msg << endl;
+int startMenu() {
+    int input = 0;
+    bool flag;
+    do {
+        string userInput;
+        cout << "Bem vindo ao sistema de armazenamento de grafos" << endl;
+        string msg = "Escolha entre as opcoes abaixo\n1)Grafo direcionado nao-ponderado\n2)Grafo nao-direcionado nao-ponderado\n3)Grafo direicionado ponderado\n4)Grafo nao-direcionado ponderado\n\n0)Para sair";
+        cout << msg << endl;
+        scanf("%d", &input);
+        flag = input < 0 && input > 4;
+
+        if (flag) {
+            cout << "\n\nOpcao invalida digite novamente\n\n" << endl;
+        }
+    }while (flag);
+    return input;
 }
 
 int main() {
-    int input = 0;
+    int input;
     do {
-        do {
-            string userInput;
-            basicMessage();
-            scanf("%d", &input);
-        }while (input < 0 && input > 4);
-//    string teste = {'A','-','>','C'};
-    } while(input != 0);
+        input = startMenu();
+    }while (input != 0);
     return 0;
 };
