14b80cafc7029cfb414a9a298f4501ff947e256d
MarcosAniCury <curymarcos@live.com>
2022-04-10 18:01:23 -0300

    Finish the work

--- Quarto Periodo/Grafos/exercise1/main.cpp
+++ Quarto Periodo/Grafos/exercise1/main.cpp
@@ -3,120 +3,46 @@
 using namespace std;
 
 #define MAX_PAIR_VETEX 8
-#define FIRST_VERTEX 1
-#define LAST_VERTEX 6
+#define FIRST_VERTEX 0
+#define LAST_VERTEX 5
 
 //Pair of vertex to build a graph
 int firstValueOfPairVertex[MAX_PAIR_VETEX] = {1, 2, 3, 4, 1, 5, 6, 2};
 int secondValueOfPairVertex[MAX_PAIR_VETEX] = {2,3,1,6,5,3,1,4};
 int weightOfPairVertex[MAX_PAIR_VETEX] = {10,2,3,5,6,4,2,9};
 
-class AdjacencyVertex {
-    int vertex;
-    int weight;
-    AdjacencyVertex *prox;
-public:
-    AdjacencyVertex() {
-        vertex = 0;
-        weight = -1;
-        prox = NULL;
-    }
-
-    int getVertex() const {
-        return vertex;
-    }
-
-    void setVertex(int vertex) {
-        AdjacencyVertex::vertex = vertex;
-    }
-
-    int getWeight() const {
-        return weight;
-    }
-
-    void setWeight(int weight) {
-        AdjacencyVertex::weight = weight;
-    }
-
-    AdjacencyVertex *getProx() const {
-        return prox;
-    }
-
-    void setProx(AdjacencyVertex *prox) {
-        AdjacencyVertex::prox = prox;
-    }
-};
-
-class AdjacencyList {
-    AdjacencyVertex *adjacencyList;
+class MatrizAdjacency{
+    int matrizAdjacency[LAST_VERTEX+1][LAST_VERTEX+1];
     bool isWeighted;
+    bool isDirection;
 public:
-    AdjacencyList() {
-        AdjacencyVertex vertex[LAST_VERTEX+1];
-        adjacencyList = vertex;
+    MatrizAdjacency(bool isWeighted, bool isDirection) {
+        MatrizAdjacency::isWeighted = isWeighted;
+        MatrizAdjacency::isDirection = isDirection;
 
-        isWeighted = false;
-    }
-
-    AdjacencyList(bool isWeighted) {
-        AdjacencyVertex vertex[LAST_VERTEX+1];
-        adjacencyList = vertex;
-
-        AdjacencyList::isWeighted = isWeighted;
-    }
-
-    AdjacencyVertex *getAdjacencyList() const {
-        return adjacencyList;
-    }
-
-    void setAdjacencyList(AdjacencyVertex *adjacencyList) {
-        AdjacencyList::adjacencyList = adjacencyList;
-    }
-
-    bool isWeighted1() const {
-        return isWeighted;
-    }
-
-    void setIsWeighted(bool isWeighted) {
-        AdjacencyList::isWeighted = isWeighted;
-    }
-
-    void setVertex(int index, int weight = -1, int proxVertex = 0) {
-        AdjacencyVertex* vertexModify = &adjacencyList[index];
-        while (vertexModify->getProx() != NULL) {
-            vertexModify = vertexModify->getProx();
-        }
-
-        if (vertexModify->getVertex() == 0) {
-            vertexModify->setVertex(index);
-        }
-
-        AdjacencyVertex vertex;
-        if (isWeighted) {
-            vertex.setWeight(weight);
-        }
-        if (proxVertex != 0) {
-            vertex.setVertex(proxVertex);
-            vertexModify->setProx(&vertex);
+        for (int i = FIRST_VERTEX;i <= LAST_VERTEX;i++) {
+            for (int j = FIRST_VERTEX; j <= LAST_VERTEX; j++) {
+                matrizAdjacency[i][j] = 0;
+            }
         }
     }
 
-    AdjacencyVertex *getVertex(int index) {
-        return &AdjacencyList::adjacencyList[index];
+    void setVertex(int indexLine, int indexCol, int weight = 1) {
+        matrizAdjacency[indexLine-1][indexCol-1] = weight;
+
+        if (!isDirection) {
+            matrizAdjacency[indexCol-1][indexLine-1] = weight;
+        }
     }
 
     void print() {
+        string line = "";
         for (int i = FIRST_VERTEX;i <= LAST_VERTEX;i++) {
-            AdjacencyVertex vertex = adjacencyList[i];
-            string msg = vertex.getVertex() + ") -> ";
-            AdjacencyVertex* prox = vertex.getProx();
-            while (prox != NULL) {
-                msg += prox->getVertex() + ',';
-                prox = prox->getProx();
+            for (int j = FIRST_VERTEX;j <= LAST_VERTEX;j++) {
+                line += to_string(matrizAdjacency[i][j]) + "|";
             }
-            msg.pop_back();
-            msg += "\n";
-            cout << msg << endl;
+            cout << line << endl;
+            line = "";
         }
     }
 };
@@ -138,55 +64,57 @@ int startMenu() {
     return input;
 }
 
-void createGraphDirectedNotWeighted() {
-    bool isWeighted = false;
-    AdjacencyList graphDirectedNotWeighted(isWeighted);
-    for(int i = 0; i < MAX_PAIR_VETEX;i++) {
+void createGraph(bool isWeighted,bool isDirected) {
+    MatrizAdjacency graph(isWeighted, isDirected);
+    for(int i = FIRST_VERTEX; i < MAX_PAIR_VETEX;i++) {
         int firstValuePairOfVertex = firstValueOfPairVertex[i];
         int secondValuePairOfVertex = secondValueOfPairVertex[i];
+        int weighted = 1;
 
-        graphDirectedNotWeighted.setVertex
+        if (isWeighted) {
+            weighted = weightOfPairVertex[i];
+        }
+
+        graph.setVertex
         (
             firstValuePairOfVertex,
-            -1,
-            secondValuePairOfVertex
+            secondValuePairOfVertex,
+            weighted
         );
     }
 
-    graphDirectedNotWeighted.print();
-}
-
-void createGraphNotDirectedNotWeighted() {
-
-}
-
-void createGraphDirectedWeighted() {
-
-}
-
-void createGraphNotDirectedWeighted() {
-
+    graph.print();
 }
 
 int main() {
     int input;
     do {
         input = startMenu();
+        bool isWeighted;
+        bool isDirected;
         switch (input) {
             case 1:
-                createGraphDirectedNotWeighted();
+                isWeighted = false;
+                isDirected = true;
+                createGraph(isWeighted,isDirected);
                 break;
 
             case 2:
-                createGraphNotDirectedNotWeighted();
+                isWeighted = false;
+                isDirected = false;
+                createGraph(isWeighted,isDirected);
                 break;
 
             case 3:
-                createGraphDirectedWeighted();
+                isWeighted = true;
+                isDirected = true;
+                createGraph(isWeighted,isDirected);
                 break;
 
             case 4:
-                createGraphNotDirectedWeighted();
+                isWeighted = true;
+                isDirected = false;
+                createGraph(isWeighted,isDirected);
                 break;
 
             default: cout << "\n\nObrigado por utilizar o sistema" << endl;
