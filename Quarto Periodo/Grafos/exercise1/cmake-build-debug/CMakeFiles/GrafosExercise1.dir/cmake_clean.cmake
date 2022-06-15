e9b426a066b68dbf8588f0803f5d5d132a4ac458
MarcosAniCury <curymarcos@live.com>
2022-06-15 20:14:39 -0300

    Add files storaged in local notebook

---
+++ Quarto Periodo/Grafos/exercise1/cmake-build-debug/CMakeFiles/GrafosExercise1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GrafosExercise1.dir/main.cpp.obj"
+  "CMakeFiles/GrafosExercise1.dir/main.cpp.obj.d"
+  "GrafosExercise1.exe"
+  "GrafosExercise1.exe.manifest"
+  "GrafosExercise1.pdb"
+  "libGrafosExercise1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GrafosExercise1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Quarto Periodo/Grafos/exercise1/cmake-build-debug/CMakeFiles/GrafosExercise1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for GrafosExercise1.
+# This may be replaced when dependencies are built.
