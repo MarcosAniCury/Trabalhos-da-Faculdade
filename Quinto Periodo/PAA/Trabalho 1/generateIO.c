082a1313f299e153285572c3cbcf50aad8f412c7
marcos <curymarcos@live.com>
2023-03-06 22:26:00 -0300

    Add new modify in work 1 PAA

--- Quinto Periodo/PAA/Trabalho 1/generateIO.c
+++ Quinto Periodo/PAA/Trabalho 1/generateIO.c
@@ -3,6 +3,40 @@
 
 int main()
 {
-    printf("Oi");
+    FILE *fp;
+    char filename[] = "GenerateIOOutput.txt";
+
+    fp = fopen(filename, "w");
+    if (fp == NULL)
+    {
+        printf("Error: failed to create file\n");
+        return 1;
+    }
+
+    int n = 0;
+    int m = 0;
+
+    printf("Digite o numero de linhas da matriz:");
+    scanf("%d", &n);
+
+    printf("Digite o numero de colunas da matriz:");
+    scanf("%d", &m);
+
+    srand(time(NULL));
+
+    int numberRandom = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            numberRandom = rand() % 255;
+            printf("%d\n", numberRandom);
+            fprintf(fp, "%d|", numberRandom);
+        }
+        fprintf(fp, "\n");
+    }
+
+    fclose(fp);
     return 0;
 }
